806a9ec26ffb71b372a20b57ebb1e2a2cc5c8ed9
pacoleija <99375277+pacoleija@users.noreply.github.com>
2022-02-19 20:06:41 -0600

    Create from2.py

---
+++ from2.py
@@ -0,0 +1,22 @@
+from numpy import true_divide
+
+
+numero1 = 500
+numero2 = 500.3
+booleanvariable = True
+print(numero1, numero2)
+
+nombre ="Abril"
+print(nombre)
+print(type(numero1),type(nombre), type(numero2), type(booleanvariable))
+#esto es un comentario
+
+print('********************')
+
+ResultadoBooleano = 1 > 0 
+print(ResultadoBooleano)
+edad = 25
+print("Felicidades", nombre,"por tus ", edad, "Años")
+
+#Interpolas texto y variables utilizar "f"
+print(f"Felicidades {nombre} por tus {edad} años")
