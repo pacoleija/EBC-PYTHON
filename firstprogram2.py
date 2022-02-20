9e5572abff80f4269363c3094536eb42830b5f31
pacoleija <99375277+pacoleija@users.noreply.github.com>
2022-02-19 20:06:20 -0600

    Create firstprogram2.py

---
+++ firstprogram2.py
@@ -0,0 +1,8 @@
+Nombre = str(input('Escribe tu nombre '))
+print(f'¡Hola {Nombre}!')
+
+Edad= int(input("Escribe tu edad: "))
+print(Edad)
+
+Precio= float(input("Precio del producto: "))
+print(Precio + Edad)
