e9928865f2b0138f20286ccce927aac91e5e9e91
pacoleija <99375277+pacoleija@users.noreply.github.com>
2022-02-19 20:09:59 -0600

    Create sumadora.py

---
+++ sumadora.py
@@ -0,0 +1,24 @@
+numero1 =int(input("Numero 1: "))
+numero2 = int(input("Numero 2: "))
+
+def suma(a,b):
+    return a+ b 
+print(suma(numero1, numero2))
+
+#Resta
+def rest(a,b):
+   return  a-b
+
+print( rest( numero1, numero2))
+
+#Multiplicación
+def mult (a,b):
+   return  a*b
+
+print( mult( numero1, numero2))
+
+#División
+def div (a,b):
+   return  a/b
+
+print( div( numero1, numero2))
