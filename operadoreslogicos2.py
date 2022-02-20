61e35fa10de134da3470a5635c04d38bd386433a
pacoleija <99375277+pacoleija@users.noreply.github.com>
2022-02-19 20:08:43 -0600

    Create operadoreslogicos2.py

---
+++ operadoreslogicos2.py
@@ -0,0 +1,27 @@
+number =50
+print( number ==50 and number >20)
+print( number ==50 and number >60)
+print( number ==50 & number >20)
+
+print('\n**************************')
+
+color = 'Yelow'
+
+print( color =='Red' or color =='Blue')
+
+def comprar ():
+    if color =='Red' or color =='Blue':
+      return'compralo'
+    else:
+      return 'no comprar'
+
+print(comprar())
+
+print('\n**************************')
+
+#Funcion Not
+print(not number ==50)
+#Función Diferente
+print(number !=50)
+
+print('\n**************************')
