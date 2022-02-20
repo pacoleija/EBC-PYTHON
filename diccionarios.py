369410c741af1c0d3d30b0aa14224f1d47df9cc4
pacoleija <99375277+pacoleija@users.noreply.github.com>
2022-02-19 20:01:52 -0600

    Create diccionarios.py

---
+++ diccionarios.py
@@ -0,0 +1,15 @@
+user= {
+    "name": {"firstname":"Francisco","lastname":"Leija"},
+    "edad":31,
+    "profesion":"Financiero"
+}
+
+#print(type(user))
+#for key in user:
+#   print(f'element[{key}]: {user[key]}')
+
+print(user["profesion"])
+print(user["name"]['firstname'])
+
+print('*******************')
+
